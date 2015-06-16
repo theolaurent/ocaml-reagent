5c4500d7d969bf58ed9d3a98218f837e227a0857
Theo Laurent <theo.laurent@ens.fr>
2015-06-16 14:09:51 +0100

    Adding a test.

--- reimplem_test.ml
+++ reimplem_test.ml
@@ -18,9 +18,22 @@ let h (type u) (x:u eff) (k:(u, 'b) cont) : 'b = match x with
   | OfString s -> continue k (int_of_string s)
   | _ -> raise Unhandled
 
-(* That print 7, which seems to be allright *)
+let h1 (type u) (x:u eff) (k:(u, 'b) cont) : 'b = match x with
+  | OfString s -> continue k (int_of_string s)
+  | _ -> continue k (perform x)
+
+let h2 (type u) (x:u eff) (k:(u, 'b) cont) : 'b = match x with
+  | OfInt i -> continue k i
+  | _ -> continue k (perform x)
+
 let _ =
-  print "%d" begin
-          tryeff f
-                 { handle = h }
-        end
+  (* That print 7, which seems to be allright *)
+  print "First  %d\n" begin
+          tryeff f { handle = h }
+        end ;
+  flush_all () ;
+  (* That print 7, but seems to be non deterministic about termination... Humm... *)
+  print "Second %d\n" begin
+          tryeff (fun () -> tryeff f { handle = h1 })
+                 { handle = h2 }
+        end ;
