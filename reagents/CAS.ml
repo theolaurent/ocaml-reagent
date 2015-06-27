d420a2cd82dec67092a42b1920cf80b0e2a4e181
Theo Laurent <theo.laurent@ens.fr>
2015-06-27 10:01:06 +0100

    Minor changes.

---
+++ reagents/CAS.ml
@@ -0,0 +1,87 @@
+
+(* TODO: afterCAS actions!! *)
+
+type 'a refstate =
+  | Normal of 'a
+  | OnGoingKCAS of 'a (* store the old value *)
+
+type 'a ref = { mutable content : 'a refstate }
+
+let ref x = { content = Normal x }
+
+let get r = match r.content with
+  | Normal x -> x
+  | OnGoingKCAS x -> x
+
+(* waiting for the hardware version *)
+let docas r ~ov ~nv =
+  (* lock? *)
+  match r.content with
+  | Normal x when x = ov -> ( r.content <- Normal nv ; true )
+  | _                    -> false
+  (* unlock? *)
+
+(* Isn't that heavy at runtime? OCaml should definetly have existentials. *)
+type 'a t_struct = { ov : 'a ; nv : 'a ; r : 'a ref }
+module type t = sig type t val it : t t_struct end
+type t = (module t)
+
+let build (type u) (r:u ref) ~(ov:u) ~(nv:u) =
+  let module M = struct
+      type t = u
+      let it = { ov ; nv ; r }
+    end in (module M : t)
+
+let commit cas =
+  let module M = (val cas : t) in
+  let { r ; ov ; nv } = M.it in
+  docas r ~ov ~nv
+
+let semicas cas =
+  let module M = (val cas : t) in
+  let { r ; ov ; _ } = M.it in
+  (* lock? *)
+  match r.content with
+  | Normal x when x = ov -> ( r.content <- OnGoingKCAS x ; true )
+  | _                    -> false
+  (* unlock? *)
+
+(* only the tread that perform the semicas should be able to rollbwd/fwd *)
+let rollbwd cas =
+  let module M = (val cas : t) in
+  let { r ; _ } = M.it in
+  (* lock? *)
+  match r.content with
+  | Normal _ -> assert false
+  | OnGoingKCAS x -> r.content <- Normal x
+  (* unlock? *)
+
+let rollfwd cas =
+  let module M = (val cas : t) in
+  let { r ; nv ; ov } = M.it in
+  (* lock? *)
+  match r.content with
+  | Normal _ -> assert false
+  | OnGoingKCAS x -> ( assert (x = ov) ; r.content <- Normal nv )
+  (* unlock? *)
+
+let kCAS l =
+  if List.for_all (fun cas -> semicas cas) l then
+    ( List.iter rollfwd l ; true )
+  else
+    ( List.iter rollbwd l ; false )
+
+module Sugar : sig
+  type 'a casref_update
+  val ( ! ) : 'a ref -> 'a
+  val ( --> ) : 'a -> 'a -> 'a casref_update
+  val ( := ) : 'a ref -> 'a casref_update -> t
+end = struct
+  type 'a casref_update = { ov : 'a ;
+                            nv : 'a }
+  let (!) r = get r
+
+  let (-->) ov nv = { ov ; nv }
+
+  let (:=) r { ov ; nv } = build r ~ov ~nv
+end
---
+++ reagents/CAS.mli
@@ -0,0 +1,24 @@
+
+type 'a ref
+
+val ref : 'a -> 'a ref
+
+val get : 'a ref -> 'a
+
+val docas : 'a ref -> ov:'a -> nv:'a -> bool
+
+type t
+
+val build : 'a ref -> ov:'a -> nv:'a -> t
+
+val commit : t -> bool
+
+val kCAS : t list -> bool
+
+module Sugar :
+  sig
+    type 'a casref_update
+    val ( ! ) : 'a ref -> 'a
+    val ( --> ) : 'a -> 'a -> 'a casref_update
+    val ( := ) : 'a ref -> 'a casref_update -> t
+  end
