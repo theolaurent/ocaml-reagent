ee5b7b54d8295bb737575e929e48b429a95d36a3
Theo Laurent <theo.laurent@ens.fr>
2015-06-30 09:18:07 +0100

    Add a few tests.

---
+++ reagents/test_queue.ml
@@ -0,0 +1,20 @@
+
+module Q = BasicConcurrentQueue
+
+let () =
+  let q = Q.create () in
+  assert (Q.try_pop q = None) ;
+  assert (Q.try_pop q = None) ;
+  Q.push 1 q ;
+  Q.push 2 q ;
+  assert (Q.try_pop q = Some 1) ;
+  Q.push 3 q ;
+  assert (Q.try_pop q = Some 2) ;
+  assert (Q.try_pop q = Some 3) ;
+  assert (Q.try_pop q = None) ;
+  Q.push 4 q ;
+  Q.push 5 q ;
+  assert (Q.try_pop q = Some 4) ;
+  assert (Q.try_pop q = Some 5) ;
+  assert (Q.try_pop q = None) ;
+  Printf.printf "OK\n" ;
