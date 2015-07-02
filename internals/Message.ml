34ca0f9b60abf2ad682b4569c6b5cacf294b327b
Theo Laurent <theo.laurent@gmx.fr>
2015-07-02 08:50:25 +0100

    Using an option type rather than this record field "isCommit".

--- internals/Message.ml
+++ internals/Message.ml
@@ -1,6 +1,7 @@
 
 open Reaction.Sugar
 open Reagent.Sugar
+open Reagent
 
 (* TODO: prevent getting twice the same message in a reaction. *)
 
@@ -15,16 +16,16 @@ let is_available (M m) =
   Offer.is_waiting m.offer
 
 let send f =
-  let tryReact arg rx next offer =
+  let withReact arg rx next offer =
     f (M { playload = arg ; senderRx = rx ; senderK = next ; offer = offer })
-  in { Reagent.tryReact = tryReact ; isCommit = false }
+  in Some { withReact }
 
 let receive (M m) =
   let merge =
-    let tryReact arg rx next offer =
-      next.Reagent.tryReact m.playload
-                            (m.senderRx ++ rx ++ Reaction.c_offer m.offer arg)
-                            Reagent.commit
-                            offer
-    in { Reagent.tryReact = tryReact ; isCommit = false }
+    let withReact arg rx next offer =
+      (commit next).withReact m.playload
+                              (m.senderRx ++ rx ++ Reaction.c_offer m.offer arg)
+                              None
+                              offer
+    in Some { withReact }
   in m.senderK >> merge
--- internals/Reagent.ml
+++ internals/Reagent.ml
@@ -5,59 +5,65 @@ open Reaction.Sugar
 (* TODO: optim block / non block                         *)
 (* TODO: optim when only one cas                         *)
 
-type ('a, 'b) t = { tryReact : 'r . 'a -> Reaction.t
-                                       -> ('b, 'r) t
-                                       -> 'r Offer.t -> unit ;
-                    isCommit : bool
-                  }
+type ('a, 'b) t_struct = { withReact : 'r . 'a -> Reaction.t
+                                              -> ('b, 'r) t
+                                              -> 'r Offer.t -> unit ;
+                         }
+and ('a, 'b) t = ('a, 'b) t_struct option
+
 
 (* TODO: find a way to explain what's going on to the type system and *)
 (*       get rid of Obj.magic                                         *)
-let rec commit : 'a . ('a, 'a) t =
-  let tryReact arg rx next offer =
-    if next.isCommit then
-      ( if !! rx
-        then ignore (Offer.try_resume offer (Obj.magic arg))
-        else failwith "Reagent.commit: No transient failure for now" )
-    else
-      (* assert false *)
-      next.tryReact arg rx commit offer
-  in { tryReact ; isCommit = true }
+let rec commit : 'a 'b . ('a, 'b) t -> ('a, 'b) t_struct = fun r ->
+  let commit_struct : ('c, 'c) t_struct =
+    let withReact arg rx next offer = match next with
+      | None -> if !! rx
+                then ignore (Offer.try_resume offer (Obj.magic arg))
+                else failwith "Reagent.commit: No transient failure for now"
+      | _    -> (commit next).withReact arg rx None offer
+    in { withReact }
+  in
+  match r with
+  | None -> Obj.magic commit_struct
+  | Some r -> r
+
+
 
 let run : ('a, 'b) t -> 'a -> 'b =
-  (fun r arg -> Offer.suspend (r.tryReact arg Reaction.inert commit))
+  (fun r arg -> Offer.suspend ((commit r).withReact arg Reaction.inert None))
 
 (* TODO: think about what happen with (swap enpoint a) >> (swap enpoint b)  *)
 (* and the channel is empty: it will block ; should it atomically exchange? *)
 (* what does the scalal version?                                            *)
 let rec pipe : 'a 'b 'c . ('a, 'b) t -> ('b, 'c) t -> ('a, 'c) t = (fun r1 r2 ->
-   let tryReact arg rx next offer =
-     r1.tryReact arg rx
-                 ( if next.isCommit then Obj.magic r2
-                   else pipe r2 next ) offer
-   in { tryReact ; isCommit = false }
+   let withReact arg rx next offer =
+     (commit r1).withReact arg rx (match (r2, next) with (* to prevent commit to loop *)
+                                   | (None, None) -> None
+                                   | _ -> pipe r2 next)
+                offer
+   in Some { withReact }
   )
 
 (* TODO: I think the build should be concurrent.                            *)
 (*       Humm wait it depend on what you want cf tempt reagent.             *)
 let choose (r1:('a, 'b) t) (r2:('a, 'b) t) : ('a, 'b) t =
-  let tryReact arg rx next offer =
-    r1.tryReact arg rx next offer ; r2.tryReact arg rx next offer
-  in { tryReact ; isCommit = false }
+  let withReact arg rx next offer =
+    (commit r1).withReact arg rx next offer ; (commit r2).withReact arg rx next offer
+  in Some { withReact }
 
 let constant (x:'a) : ('b, 'a) t =
-  let tryReact _ rx next offer =
-    next.tryReact x rx commit offer
-  in { tryReact ; isCommit = false }
+  let withReact _ rx next offer =
+    (commit next).withReact x rx None offer
+  in Some { withReact }
 
 let never : ('a, 'b) t =
-  let tryReact _ _ _ _ = ()
-  in { tryReact ; isCommit = false }
+  let withReact _ _ _ _ = ()
+  in Some { withReact }
 
 let noop : ('a, 'a) t =
-  let tryReact arg rx next offer =
-    next.tryReact arg rx commit offer
-  in { tryReact ; isCommit = false }
+  let withReact arg rx next offer =
+    (commit next).withReact arg rx None offer
+  in Some { withReact }
 
 
 (* read ref should not be a reagent. Indeend it can *)
@@ -65,9 +71,9 @@ let noop : ('a, 'a) t =
 (* equivalent to const (!r)                         *)
 
 let cas (r:'a casref) (updt:'a casupdt) : (unit, unit) t =
-  let tryReact () rx next offer =
-    next.tryReact () (rx ++ Reaction.cas (r <:= updt)) commit offer
-  in { tryReact ; isCommit = false }
+  let withReact () rx next offer =
+    (commit next).withReact () (rx ++ Reaction.cas (r <:= updt)) None offer
+  in Some { withReact }
 (* TODO: Hmm but the actual cas will not be performed until the commit phase ..    *)
 (* Thus, two cas piped are bound to fail? (the problem being cas >> read is false! *)
 (* I think this version behave as the scala code thought  ..                       *)
@@ -79,27 +85,27 @@ let cas (r:'a casref) (updt:'a casupdt) : (unit, unit) t =
 (* Be careful with lift and computed, their behaviour can be non-    *)
 (* intuistic when sequenced. Remember that a reaction is two-phased. *)
 let lift (f:'a -> 'b) : ('a, 'b) t =
-  let tryReact arg rx next offer =
-    next.tryReact (f arg) rx commit offer
-  in { tryReact ; isCommit = false }
+  let withReact arg rx next offer =
+    (commit next).withReact (f arg) rx None offer
+  in Some { withReact }
 
 let computed (f:('a -> (unit, 'b) t)) : ('a, 'b) t =
-  let tryReact arg rx next offer =
-    (f arg).tryReact () rx next offer
-  in { tryReact ; isCommit = false }
+  let withReact arg rx next offer =
+    (commit (f arg)).withReact () rx next offer
+  in Some { withReact }
 
 
 (* TODO: I am not fully satistied with the pair, I think the built up of *)
 (* the reaction might be concurrent... (not as in the scala version!)    *)
 let first (r:('a, 'b) t) : ('a * 'c, 'b * 'c) t =
-  let tryReact (a, c) rx next offer =
-    (pipe r (lift (fun b -> (b, c)))).tryReact a rx next offer
-  in { tryReact ; isCommit = false }
+  let withReact (a, c) rx next offer =
+    (commit (pipe r (lift (fun b -> (b, c))))).withReact a rx next offer
+  in Some { withReact }
 
 let second (r:('a, 'b) t) : ('c * 'a, 'c * 'b) t =
-  let tryReact (c, a) rx next offer =
-    (pipe r (lift (fun b -> (c, b)))).tryReact a rx next offer
-  in { tryReact ; isCommit = false }
+  let withReact (c, a) rx next offer =
+    (commit (pipe r (lift (fun b -> (c, b))))).withReact a rx next offer
+  in Some { withReact }
 
 let pair (r1:('a, 'b) t) (r2:('a, 'c) t) : ('a, ('b * 'c)) t =
   pipe (lift (fun a -> (a, a)))
