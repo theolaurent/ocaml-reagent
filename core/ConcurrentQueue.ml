addbeda9ad811b97f98fe24334acfa769c045fc2
Theo Laurent <theo.laurent@ens.fr>
2015-07-05 18:14:26 +0100

    New non blocking concurrent queue. Supposed to be more efficient...

--- core/ConcurrentQueue.ml
+++ core/ConcurrentQueue.ml
@@ -2,45 +2,58 @@
 open CAS.Sugar
 open Reagent.Sugar
 
-(* Concurrent queue based on functionnal queue stored in a CAS reference. *)
-(* Not the most efficient concurrent queue, but has avantages, like       *)
-(* returning the whole content without copying it!                        *)
-(* This queue is non-blocking, it only fail trasiently.                   *)
+type 'a node = N of ('a * 'a node) option casref
 
+type 'a t = { head : 'a node ; tail : 'a node }
+(* invariant : head = None <=> tail = None          *)
+(* invariant : tail = Some (x, node) => node = None *)
 
-type 'a t = 'a FQueue.t casref
-
-
-let create () = ref FQueue.empty
-
-let fold f q = FQueue.fold_right f !q
+let create () = { head = N (ref None) ; tail = N (ref None) }
 
 let push q =
-  Reagent.computed (fun v -> let s = !q in
-                             Reagent.cas q (s --> FQueue.push v s))
+  Reagent.computed (fun v ->
+    let newnode = Some (v, N (ref None)) in
+    let N head = q.head in
+    let N tail = q.tail in
+    let s = !(tail) in
+    match s with
+    | None             -> Reagent.cas head (None --> newnode)
+                       |> Reagent.cas tail (None --> newnode)
+    | Some (_, N node) -> assert (!node = None) ;
+                          Reagent.cas tail (s    --> newnode)
+                       |> Reagent.cas node (None --> newnode)
+  )
 
+(* TODO: I feel this pop reagent could be written in a more elegant way *)
 let pop q =
-  Reagent.computed (fun () -> try
-                                let s = !q in
-                                let (v, n) = FQueue.pop s in
-                                Reagent.cas q (s --> n)
-                                |> Reagent.constant v
-                              with FQueue.Empty -> Reagent.retry)
+  Reagent.computed (fun () ->
+    let N head = q.head in
+    let N tail = q.tail in
+    let s = !(head) in
+    match s with
+    | None -> Reagent.retry (* non blocking queue *)
+    | Some (v, N node) ->
+       let s' = !node in
+       begin match s' with
+             | None   -> Reagent.cas head (s --> None)
+                      |> Reagent.cas tail (s --> None)
+                      |> Reagent.constant v
+             | Some _ -> Reagent.cas head (s --> s'  )
+                      |> Reagent.constant v
+       end
+  )
 
-let pop_until q f =
-  let rec loop fq =
-    try
-      let (v, n) = FQueue.pop fq in
-      if not (f v) then loop n
-      else fq
-    with FQueue.Empty -> fq
-  in
-  Reagent.computed (fun () -> let s = !q in
-                              Reagent.cas q (s --> loop s))
+let rec pop_until q f =
+     Reagent.attempt (pop q)
+  |> Reagent.computed (fun x -> match x with
+       | None   -> Reagent.constant ()
+       | Some v when not (f v)
+                -> Reagent.post_commit (Reagent.run (pop_until q f))
+                  (* use post commit to pop 1-by-1, more efficient that kCAS *)
+       | Some _ -> Reagent.never
+  )
+type 'a cursor = 'a node
 
+let snapshot q = q.head
 
-type 'a cursor = 'a FQueue.t
-
-let snapshot q = !q
-
-let next c = try Some (FQueue.pop c) with FQueue.Empty -> None
+let next (N c) = !c
