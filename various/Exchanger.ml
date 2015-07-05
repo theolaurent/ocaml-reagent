4bdf2b3a504ffe0484c878412bf68bb607791e21
Theo Laurent <theo.laurent@ens.fr>
2015-07-05 22:05:34 +0100

    A tiny comment.

--- various/Exchanger.ml
+++ various/Exchanger.ml
@@ -5,4 +5,5 @@ type 'a t = (('a, 'a) Channel.endpoint * ('a, 'a) Channel.endpoint)
 
 let create = Channel.create
 
+(* TODO: it would be better with a symmetric choose? *)
 let exchange e = Channel.swap (fst e) || Channel.swap (snd e)
