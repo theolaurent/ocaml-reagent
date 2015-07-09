53b6d41477432502f74fe9f66e6c6e18e73e74bc
Theo Laurent <theo.laurent@ens.fr>
2015-07-09 13:46:25 +0100

    Use a proper concurrent queue algorithm.

--- core/Channel.ml
+++ core/Channel.ml
@@ -5,27 +5,27 @@ open Reagent.Sugar
 (* sides of a channel?                                       *)
 (* TODO: has_offer *)
 
-type ('a, 'b) endpoint = { outgoing : ('a, 'b) Reagent.message ConcurrentQueue.t ;
-                           incoming : ('b, 'a) Reagent.message ConcurrentQueue.t }
+type ('a, 'b) endpoint = { outgoing : ('a, 'b) Reagent.message MSQueue.t ;
+                           incoming : ('b, 'a) Reagent.message MSQueue.t }
 
 let flip_endpoint e = { outgoing = e.incoming ;
                         incoming = e.outgoing }
 
 let create () =
-  let e1 = { outgoing = ConcurrentQueue.create () ;
-             incoming = ConcurrentQueue.create () } in
+  let e1 = { outgoing = MSQueue.create () ;
+             incoming = MSQueue.create () } in
   (e1, flip_endpoint e1)
 
 
 let swap e =
-  let () = Reagent.run (ConcurrentQueue.pop_until e.incoming
+  let () = Reagent.run (MSQueue.pop_until e.incoming
                                                   Reagent.is_message_available) () in
   let rec all_messages c =
-    match ConcurrentQueue.next c with
+    match MSQueue.next c with
     | None -> Reagent.never
     | Some (m, nc) -> (* use Reagent.computed to get lazyness! *)
        Reagent.computed (fun a -> Reagent.constant a >>> (    Reagent.answer m
                                                           >+> all_messages nc  ))
   in
-  let push_message m = Reagent.run (ConcurrentQueue.push e.outgoing) m in
-  all_messages (ConcurrentQueue.snapshot e.incoming) >+> Reagent.send push_message
+  let push_message m = Reagent.run (MSQueue.push e.outgoing) m in
+  all_messages (MSQueue.snapshot e.incoming) >+> Reagent.send push_message
